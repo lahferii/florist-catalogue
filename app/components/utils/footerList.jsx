54c5aeaccaaa00e8c1ae696b971d28e1667fa23b
lahferii <ferinaise@gmail.com>
2025-12-14 20:17:39 +0700

    rev in foooter padding

--- app/components/utils/footerList.jsx
+++ app/components/utils/footerList.jsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 function FooterListWrapper({title, caption, children}){
     return(
-        <div className={"mb-5 md:w-72"}>
+        <div className={"mb-2 md:w-72"}>
             <h3 className="text-xl tracking-wide mb-2">{title}</h3>
             <p>
                 {caption}
