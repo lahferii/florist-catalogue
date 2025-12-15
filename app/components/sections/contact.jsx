e0eb55c3087c9ab1df9a2e07a7415b848d659f2e
lahferii <ferinaise@gmail.com>
2025-12-15 12:28:04 +0700

    change contact section

--- app/components/sections/contact.jsx
+++ app/components/sections/contact.jsx
@@ -1,19 +1,21 @@
 export default function Contact(){
     return(
-        <section className="mb-20 relative">
-            <article className="mb-5 text-center">
-                <h2 className="text-3xl tracking-wider mb-2">Ingin Mengenal Kami Lebih Dekat?</h2>
-                <p>Kami berkomitmen memberikan pelayanan terbaik dengan kualitas premium</p>
-            </article>
+        <section className="mb-20 relative border-2 border-rose-500 p-1">
+            <div className="w-full h-full bg-rose-500 py-20">
+                <article className="mb-5 text-center text-white">
+                    <h2 className="text-3xl tracking-wider mb-2">Ingin Mengenal Kami Lebih Dekat?</h2>
+                    <p>Kami berkomitmen memberikan pelayanan terbaik dengan kualitas premium</p>
+                </article>
 
-            <article className="flex flex-row gap-5 justify-center">
-                <div>
-                <button className="btn-primary py-2">Hubungi Kami</button>
-                </div>
-                <div>
-                <button className="btn-secondary py-2">Lihat Kami Di Shopee</button>
-                </div>
-            </article>
+                <article className="flex flex-row gap-5 justify-center">
+                    <div>
+                    <button className="btn-secondary border-2 border-rose-500 py-2">Hubungi Kami</button>
+                    </div>
+                    <div>
+                    <button className="btn-secondary bg-rose-500 py-2 border-2 border-white text-white">Lihat Kami Di Shopee</button>
+                    </div>
+                </article>
+            </div>
         </section>
     )
 }
